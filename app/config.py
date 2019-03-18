d3115d5c459ffad58f07c7b3dd6e0eb132ebbac6
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2019-03-18 14:56:32 +0000

    Formatting

--- app/config.py
+++ app/config.py
@@ -5,9 +5,11 @@ import os
 # TODO: change these things to an instance path
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL"
+    ) or "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-

ee34e22cb97132289039e190810a81ca692a664a
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2019-03-18 16:59:58 +0000

    Use dotenv package

--- config.py
+++ config.py
@@ -2,9 +2,11 @@
 
 import os
 
+from dotenv import load_dotenv
+
 # TODO: change these things to an instance path
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"

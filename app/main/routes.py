df42a47ea20ec14a5716b5b23d4595239a260114
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2019-03-19 16:19:00 +0000

    add task overview to user landing page

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from flask import render_template
-from flask_login import login_required
+from flask_login import current_user
 
 from app.main import bp
+from app.models import Task
+
 
 @bp.route("/")
 @bp.route("/index")
 def index():
+    if current_user.is_authenticated:
+        user_id = current_user.id
+        tasks = Task.query.filter_by(annotator_id=user_id).all()
+        tasks_done = [t for t in tasks if t.done]
+        tasks_todo = [t for t in tasks if not t.done]
+        return render_template("index.html", title="Home", 
+                tasks_done=tasks_done, tasks_todo=tasks_todo)
     return render_template("index.html", title="Home")

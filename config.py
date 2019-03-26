bfb250ad3f0150a8aaa0a0c28aee2c31ba2b7522
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2019-03-26 14:51:00 +0000

    Use gmail address as admin

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['gvandenburg@turing.ac.uk']
+    ADMINS = ["annotatechange@gmail.com"]
 
     # these should be used relative to the instance path
     DATASET_DIR = "datasets"

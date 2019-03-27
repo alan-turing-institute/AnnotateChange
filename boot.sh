873f35bb1ae57ff8b40cf06fdec684545f6be81e
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2019-03-27 16:57:09 +0000

    Make boot script more robust to database being offline

--- boot.sh
+++ boot.sh
@@ -4,7 +4,13 @@
 poetry shell
 
 # Run database migrations
-flask db upgrade
-
+while true; do
+	flask db upgrade
+	if [[ "$?" == "0" ]]; then
+		break;
+	fi
+	echo "Upgrade command failed, retrying in 5 seconds ..."
+	sleep 5
+done
 
 exec gunicorn -b :80 --access-logfile - --error-logfile - annotate_change:app

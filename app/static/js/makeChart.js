048faedad22d1813f9217400ed238375ab8a457f
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2019-09-05 16:24:06 +0100

    Keep counting X values when a missing value is seen
    Otherwise we won't actually have gaps in the data!

--- app/static/js/makeChart.js
+++ app/static/js/makeChart.js
@@ -8,6 +8,7 @@ function preprocessData(data) {
 	for (i=0; i<data.values[0].raw.length; i++) {
 		d = data.values[0].raw[i];
 		if (isNaN(d)) {
+			n++;
 			if (run.length > 0)
 				cleanData.push(run);
 			run = [];
